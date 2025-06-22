fa1e67419396c37980084312bc0fa8cae0f21a89
Gimpy3887 <gimpy2345@gmail.com>
2025-06-22 17:31:54 -0400

    Add seo meta tags

--- app.vue
+++ app.vue
@@ -1,4 +1,13 @@
 
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Killzone: Mercenary Leaderboard – PS Vita Online Rankings',
+  ogTitle: 'Killzone: Mercenary Leaderboard – PS Vita Online Rankings',
+  description: 'See rankings and player stats provided by Vita4Ever for Killzone: Mercenary on PS Vita. Track your performance and try to get filthy rich.',
+  ogDescription: 'See rankings and player stats provided by Vita4Ever for Killzone: Mercenary on PS Vita. Track your performance and try to get filthy rich.',
+  // ogImage: 'https://example.com/image.png',
+})
+</script>
 <template>
   <div>
     <!-- Markup shared across all pages, ex: NavBar -->
